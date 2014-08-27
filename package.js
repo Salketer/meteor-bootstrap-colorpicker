66f674d8e74facc3f84bdf76293bb8c9acebc44f
Nathan Strauser <nathan.strauser@gmail.com>
2014-08-27 11:21:17 -0400

    meteor 0.9.0

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-    summary: "Bootstrap Colorpicker packaged for meteor"
+    name: "natestrauser:bootstrap-colorpicker",
+    summary: "Bootstrap Colorpicker packaged for meteor",
+  version: "0.1.1",
+  git: "https://github.com/nate-strauser/meteor-bootstrap-colorpicker.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
     api.add_files([
         "lib/bootstrap-colorpicker/js/bootstrap-colorpicker.js",
         "lib/bootstrap-colorpicker/css/bootstrap-colorpicker.css",
